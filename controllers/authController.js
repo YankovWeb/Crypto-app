e20b20f916a7c368ea2f16c8b183db44f40caf74
Grigor Yankov <yankovweb@outlook.com>
2023-02-14 16:40:46 +0200

    [future]DataBaseSchima

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,4 +9,7 @@ router.get("/register", (req, res) => {
   res.render("auth/register");
 });
 
+router.post("/register", (req, res) => {
+  const {username, email, password, confirmPassword} = req.body;
+});
 module.exports = router;
